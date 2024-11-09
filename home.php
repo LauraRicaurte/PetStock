d34a89ff16837686b3f0feae5a2b4cbc07fdffe4
Laura <iricaurte2002@gmail.com>
2024-11-09 11:35:02 -0500

    Sistema de Inventario PetStock.

--- home.php
+++ home.php
@@ -6,15 +6,18 @@
 <?php include_once('layouts/header.php'); ?>
 <div class="row">
   <div class="col-md-12">
-    <?php echo display_msg($msg); ?>
+    <!-- Estilizamos el mensaje -->
+    <div style="text-align: center; font-size: 20px; font-weight: bold; color: #4CAF50; margin: 20px 0;">
+      <?php echo display_msg($msg); ?>
+    </div>
   </div>
- <div class="col-md-12">
+  <div class="col-md-12">
     <div class="panel">
       <div class="jumbotron text-center">
-         <h1>Esta es su nueva página de inicio</h1>
-     
+         <!-- Mostramos la imagen -->
+         <img src="uploads/imagenes/mascotas.jpg" alt="Inicio" class="img-fluid" style="max-width: 100%; height: auto;">
       </div>
     </div>
- </div>
+  </div>
 </div>
 <?php include_once('layouts/footer.php'); ?>
